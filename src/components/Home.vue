49dcb82b71e9b27a60262356e2a718ef785d73aa
Arco Mul <me@arcomul.nl>
2018-05-16 14:30:07 +0200

    Clear results on empty query

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,11 @@
       </div>  
       <form class="search">
         <div>
-          <input type="search" v-model="query" @input="onQuery" id="mySearch" name="q">
+          <input type="search"
+            :value="query"
+            @input="e => onQuery(e.target.value)"
+            @keypress="e => onQuery(e.target.value)"
+            autocomplete="off" />
         </div>
       </form>
     </div>
@@ -16,6 +20,14 @@
 
 <script>
 import { request } from 'graphql-request'
+
+const delay = (function () {
+  let timer = 0
+  return (callback, ms) => {
+    clearTimeout(timer)
+    timer = setTimeout(callback, ms)
+  }
+})()
 
 export default {
   name: 'home',
@@ -26,15 +38,25 @@ export default {
     }
   },
   methods: {
-    async onQuery () {
-      const { query } = this
-      this.results = (await request('http://rankabrand-api.arcomul.nl/graphql',
-        `{
-            queryRankings(query: "${query}") {
-                _id
-                name
-            }
-        }`)).queryRankings
+    async onQuery (query) {
+      query = query.trim()
+      this.query = query
+
+      delay(async () => {
+        // No query anymore? Clear result list
+        if (!query || query === '') {
+          this.results = []
+          return
+        }
+
+        this.results = (await request('http://rankabrand-api.arcomul.nl/graphql',
+          `{
+                queryRankings(query: "${query}") {
+                    _id
+                    name
+                }
+          }`)).queryRankings
+      }, 100)
     },
     highlight (text, highlight) {
       const t = text.replace(new RegExp(highlight, 'i'), `<span>$&</span>`)
@@ -78,18 +100,18 @@ export default {
 }
 
 li {
-    background: white;
-    padding: 1em;
-    border: .5px solid lightgrey;
     list-style: none;
-    text-align: left;
 }
 
 a {
+    display: block;
+    background: white;
+    text-align: left;
+    padding: 1em;
+    border: .5px solid lightgrey;
     text-decoration: none;
     color: grey;
     font-weight: 700;
-
 }
 
 .search {
@@ -111,6 +133,6 @@ input[type=search] {
 
 <style>
 li.search-result a span {
-  font-weight: bold;
+    color: black;
 }
 </style>
